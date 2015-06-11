0bc97e3460d2bf5ba6f052eb846875bdf625ea69
Alexandra-andreevna <Alexandra25andreevna@gmail.com>
2015-06-11 09:02:15 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,16 +1,13 @@
 #ifndef __PLAYER_H
 #define __PLAYER_H
 
-#include"Field.h"
-
-class Player: public Field
-
+class Player
 {
 public:
-	Player():Field()
+	Player()
 	{};
 	void put_ship();
 	void move(int p);
 };
-
+ 
 #endif
